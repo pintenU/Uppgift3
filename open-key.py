9197e644cab577f1009800bd9982fc375e0625ef
wrangman <wrangman@gmail.com>
2024-09-04 09:52:30 +0200

    test

---
+++ open-key.py
@@ -0,0 +1,58 @@
+import csv
+import os
+import locale
+
+def load_data(filename): 
+    products = [] 
+    
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            id = int(row['id'])
+            name = row['name']
+            desc = row['desc']
+            price = float(row['price'])
+            quantity = int(row['quantity'])
+            
+            products.append(
+                {                   
+                    "id": id,       
+                    "name": name,
+                    "desc": desc,
+                    "price": price,
+                    "quantity": quantity
+                }
+            )
+    return products
+    
+def get_product(id, products):
+    return f"{products[id]['name']} \t {products[id]['price']}"
+    
+def get_products(products):
+    product_list = []
+    for product in products:
+        product_info = f"{product['id']}) {product['name']} \t {product['desc']} \t {locale.currency(product['price'], grouping=True)}"
+        product_list.append(product_info)
+    
+    return "\n".join(product_list)
+
+
+#TODO: gör om så du slipper använda global-keyword (flytta inte "product = []")
+#TODO: skriv en funktion som returnerar en specifik produkt med hjälp av id
+
+
+locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')  
+
+os.system('cls')
+products = load_data('db_products.csv')
+
+
+print(get_products(products))
+
+id = int(input("vilken produkt vill du visa? "))
+
+os.system('cls')
+
+print(get_product(id, products))
+
+
