12dc630aa833122da66deef0a60f857d4d770cb7
wrangman <wrangman@gmail.com>
2024-09-10 09:15:19 +0200

    aDF

--- open-test.py
+++ open-test.py
@@ -28,37 +28,25 @@ def load_data(filename):
 def get_product(id, products):
     return f"{products[id]['name']} \t {products[id]['price']}"
     
-<<<<<<< HEAD
 def view_products(products):
     #TODO: gör så listan visar en nummerlista
     
     product_list = []
     for product in products:
         product_info = f"(#{product['id']}) {product['name']} \t {product['desc']} \t {locale.currency(product['price'], grouping=True)}"
-=======
-def get_products(products):
-    product_list = []
-    for product in products:
-        product_info = f"{product['id']}) {product['name']} \t {product['desc']} \t {locale.currency(product['price'], grouping=True)}"
->>>>>>> 1df897f8b1e4519c3bbac0face2aaa981c76cd86
         product_list.append(product_info)
     
     return "\n".join(product_list)
 
 
 #TODO: gör om så du slipper använda global-keyword (flytta inte "product = []")
-<<<<<<< HEAD
 #TODO: gör så man kan se en numrerad lista som börjar på 1.
 #TODO: skriv en funktion som returnerar en specifik produkt med hjälp av id
 #TODO: skriv en funktion som tar bort en specifik produkt med hjälp av id
-=======
-#TODO: skriv en funktion som returnerar en specifik produkt med hjälp av id
->>>>>>> 1df897f8b1e4519c3bbac0face2aaa981c76cd86
 
 
 locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')  
 
-<<<<<<< HEAD
 products = load_data('db_products.csv')
 
 
@@ -69,18 +57,5 @@ while True:
     id = int(input("vilken produkt vill du ta bort? "))
 
 
-=======
-os.system('cls')
-products = load_data('db_products.csv')
-
-
-print(get_products(products))
-
-id = int(input("vilken produkt vill du visa? "))
-
-os.system('cls')
-
-print(get_product(id, products))
->>>>>>> 1df897f8b1e4519c3bbac0face2aaa981c76cd86
 
 
