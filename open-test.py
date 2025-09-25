8302195b00b0de9df624228f149169e500117153
PintenU <Alexanderpinter@telia.com>
2025-09-25 11:51:16 +0200

    Uppgift_4_klar

--- open-test.py
+++ open-test.py
@@ -5,6 +5,7 @@ import locale
 
           #lista
 
+
 def format_currency(value):
     return locale.currency(value,grouping=True)
 
@@ -12,9 +13,28 @@ def list_products(products):
     for idx, product in enumerate(products, 1):
         print(f"{idx}: {product['name']} {product['price']}")
 
+def add_product(products):
+    id = int(input("välj id: "))
+    name = input("Namn: ")
+    desc = input("Beskrivning: ")
+    price = float(input("Pris:n "))
+    quantity = int(input("kvantitet: "))
+
+    products.append(
+            {
+            "id": id,
+            "name": name,
+            "desc": desc,
+            "price": price, 
+            "quantity": quantity
+            }
+    )
+    return products
+
 
 def view_products(idx, products):
     product = products[idx - 1]
+    print(f"product: {product['name']} {product['desc']} {product['quantity']}")
     return product
 
 def load_data(filename): 
@@ -41,7 +61,6 @@ def load_data(filename):
     return products
 
 
-#TODO: skriv en funktion som tar bort en specifik produkt med hjälp av id
 
 os.system('cls')
 locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')  
@@ -52,10 +71,14 @@ products = load_data('db_products.csv')
 
 while True:
     list_products(products)
+    print("Vill du visa en product(1) eller vill du lägga till en product(2)")
+    option = int(input())
 
-    idx = int(input("Välj product med nummer: "))
+    if option ==1:
+        idx = int(input("Välj product med nummer: "))
+        product = view_products(idx, products)
+        input()
 
-    product = view_products(idx, products)
-    print(f"product: {product['name']} {product['desc']}")
+    elif option == 2:
+        add_product(products)
 
-    input()
